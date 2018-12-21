a5df7730f9067e318dcfa31525aa6a358a5412e8
unknown <nbatre1@yahoo.com>
2018-12-21 17:56:20 -0600

    keep moving forward

--- sources/app.js
+++ sources/app.js
@@ -1,5 +1,14 @@
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function(){
-    getPerson()
+    getPerson(getData)
 });
+
+function getPerson(){
+    console.log("I am function");
+}
+
+
+function getData(){
+    console.log("I am the data function");
+}
