e6cb66d0806293fbcb842bf58b850b5ecc6e46f5
unknown <nbatre1@yahoo.com>
2018-12-22 05:30:33 -0600

    keep movingforward

--- sources/app.js
+++ sources/app.js
@@ -1,42 +1,43 @@
 const btn = document.getElementById('btn');
-
-btn.addEventListener('click', function(){
-    getPerson(getData)
+btn.addEventListener("click", function(){
+    getPerson(getData);
 });
 
 function getPerson(cb){
     const url ='https://randomuser.me/api/';
     const ajax = new XMLHttpRequest();
 
-    ajax.open('GET',url,true);
-    console.log("I am function");
+    ajax.open("GET", url, true);
+    
 
  ajax.onload = function(){
      if(this.status === 200){
-         cb(this.responseText,showData);
+         cb(this.responseText, showData);
        
      }
      else{
-       this.onerror()
+       this.onerror();
      
      }
- }
+ };
 
     ajax.onerror = function(){
         console.log('There was an error');
 
-    }
+    };
 
     ajax.send();
 }
 
 
-function getData(response,cb){
-    const data = JSON.parse(response);
-    console.log("I am the data function");
-    cb(data)
+function getData(response, cb){
+    data = JSON.parse(response);
+
+    const{ name:{first},name:{last},picture:{large},location:{street},phone,email }= data.results[0];
+    cb(data);
 }
 
 function showData(){
-     console.log("show data");
+  
+     console.log(data);
 }
